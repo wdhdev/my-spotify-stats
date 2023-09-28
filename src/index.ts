f80dc75c5ce4edd5c594958b4bc79fd3e16f3c39
William Harrison <william@williamdavidharrison.com.au>
2023-09-28 12:50:10 +0800

    feat: change update time to 2 mins

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ client.on("ready", () => {
     console.log(`Logged in as: ${client.user?.tag}`);
 
     postStats(client, Discord);
-    setInterval(() => { postStats(client, Discord) }, 300000); // Update embed every 5 minutes
+    setInterval(() => { postStats(client, Discord) }, 2 * 60 * 1000); // Update embed every 2 minutes
 })
 
 client.login(process.env.token);
